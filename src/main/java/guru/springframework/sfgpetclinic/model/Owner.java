707a4841a541b0a0d17a02e6c8dedd8cbca10d8f
javalenjara <Javalen git2016>
2020-04-25 13:03:15 -0500

    Adding POJOs. Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by javalenja on 25-Apr-20
+ */
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by javalenja on 25-Apr-20
+ */
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
