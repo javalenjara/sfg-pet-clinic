25147243ec8f3705823327690cade7b189e6b00f
javalenjara <Javalen git2016>
2020-05-24 17:54:57 -0500

    Added owners list and updated the owners controller. Closes #16

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -29,13 +29,14 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Fiona");
-        owner1.setLastName("Glenanne");
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
 
         ownerService.save(owner2);
 
         System.out.println("Loaded owners...");
+
 
         Vet vet1 = new Vet();
         vet1.setId(1L);
