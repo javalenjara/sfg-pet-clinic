ab4f1c6fdc035f678ba81607a37e0f94c48f3531
javalenjara <Javalen git2016>
2020-06-07 19:26:23 -0500

    Added Visit entity, updated Owner with pet relationship. Pet Type and Pet entities already exist. Closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,8 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by javalenja on 25-Apr-20
  */
 
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
